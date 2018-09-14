a38fa83e613e857bbf42a7a5bef20eab124195f8
iH8Java <19mzhang@student.dasd.org>
2018-09-14 14:01:46 -0400

    let's save some time

---
+++ src/p.java
@@ -0,0 +1,14 @@
+//i've decided that typing sout tab takes too much time
+//p.p() is sout tab
+//it can take multiple arguments and prints them each in a new line
+//p.i() is System.out.print()
+public class p {
+    public static void i (String a){
+        System.out.print(a);
+    }
+
+    public static void p (Object ... b){
+        for (Object c: b)
+            System.out.println(c);
+    }
+}
---
+++ src/test.java
@@ -0,0 +1,10 @@
+public class test {
+
+    public static void main(String[] args) {
+        p.p("line 1");
+        p.p(6);
+        p.p("a", "b", "c", "c", "c", 5);
+        p.i("test");
+        p.i("hi");
+    }
+}
